004b776aa39dca73948fc99217d2f7c552145e17
Chanda Haritha <harithachanda20@gmail.com>
2026-09-19 21:07:07 +0530

    Remove old nested project structure

--- portfolio-backend/src/main/java/com/haitha/portfolio_backend/repository/ContactMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.haitha.portfolio_backend.repository;
-
-import com.haitha.portfolio_backend.entity.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactMessageRepository
-        extends JpaRepository<ContactMessage, Long> {
-
-}
